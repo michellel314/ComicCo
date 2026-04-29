2141a0066b4e77280baee1b65381648761fc0527
11 <mobileappdev@bths.edu>
2026-04-29 14:11:49 -0400

    Initial Commit

---
+++ ComicCo/ComicCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicCoApp.swift
+//  ComicCo
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ComicCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
