21a614742dbfa7bdc2fd5eb3169e3b336f5c4617
11 <mobileappdev@bths.edu>
2026-04-29 14:13:17 -0400

    Just initial commit with a placeholder to set up the comic app

--- ComicCo/ContentView.swift
+++ ComicCo/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("It's time for comics!") // just a placeholder
         }
         .padding()
     }
