c68ac3e17f8f0be7e696531f5ef427c8eb3bf699
BT_C09_03 <mobileappdev@bths.edu>
2026-05-05 14:42:08 -0400

    I have added le ComicDescription (we shall use it to display comic details)

---
+++ ComicCo/ComicDescription.swift
@@ -0,0 +1,17 @@
+//
+//  ComicDescription.swift
+//  ComicCo
+//
+//  Created by Student on 5/5/26.
+//
+import SwiftUI
+
+struct ComicDescription: View {
+    
+    let comic: Comic
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
