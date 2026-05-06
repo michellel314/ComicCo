f0a93c844bb8713255140cc1cd5ab3651c4ad503
11 <mobileappdev@bths.edu>
2026-05-06 14:31:07 -0400

    Untrack Xcode user-specific data

---
+++ ComicCo/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  ComicCo
+//
+//  Created by Student on 5/6/26.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
