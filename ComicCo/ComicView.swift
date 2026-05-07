a332545001e280c8aec9a8cde348fca929c8bb58
11 <mobileappdev@bths.edu>
2026-05-07 14:42:49 -0400

    Updated Homepage and modified it to have animation as the starting screen with a search bar

--- ComicCo/ComicView.swift
+++ ComicCo/ComicView.swift
@@ -25,3 +25,5 @@ struct ComicView: View {
     }
 }
 
+
+
--- ComicCo/Homepage.swift
+++ ComicCo/Homepage.swift
@@ -8,32 +8,87 @@
 import SwiftUI
 
 struct Homepage: View {
+    @State private var searchText = ""
+    @State private var rotate = false
+    @Environment(DataManager.self) private var dataManager
+    
+    let colors: [Color] = [
+            .red, .orange, .yellow,
+            .green, .blue, .purple, .pink
+        ]
+    
     var body: some View {
-        HStack {
-            Text("C")
-                .foregroundStyle(.red)
-                .fontWeight(.bold)
-            Text("O")
-                .foregroundStyle(.orange)
-                .fontWeight(.bold)
-            Text("M")
-                .foregroundStyle(.green)
-                .fontWeight(.bold)
-            Text("I")
-                .foregroundStyle(.blue)
-                .fontWeight(.bold)
-            Text("C")
-                .foregroundStyle(.purple)
-                .fontWeight(.bold)
+        NavigationStack{
+            ZStack{
+                ZStack {
+                    ForEach(0..<12){ index in
+                        RoundedRectangle(cornerRadius: 6)
+                            .fill(colors[index % colors.count])
+                            .frame(width: 35, height: 35)
+                        
+                            // makes them form a circle
+                            .offset(y: -220)
+                        
+                            // rotates each one evenly
+                            .rotationEffect(
+                                .degrees(Double(index) * 30)
+                            )
+                    }
+                }
+                .rotationEffect(
+                    .degrees(rotate ? 360 : 0)
+                )
+                .animation(
+                    .linear(duration: 20)
+                    .repeatForever(autoreverses: false),
+                    value: rotate
+                )
+                .opacity(0.3)
+                VStack(alignment: .center){
+                    HStack {
+                        Text("C")
+                            .foregroundStyle(.red)
+                            .fontWeight(.bold)
+                        Text("O")
+                            .foregroundStyle(.orange)
+                            .fontWeight(.bold)
+                        Text("M")
+                            .foregroundStyle(.green)
+                            .fontWeight(.bold)
+                        Text("I")
+                            .foregroundStyle(.blue)
+                            .fontWeight(.bold)
+                        Text("C")
+                            .foregroundStyle(.purple)
+                            .fontWeight(.bold)
+                        
+                        Text("O")
+                            .foregroundStyle(.pink)
+                            .fontWeight(.bold)
+                        Text(".")
+                    }
+                    TextField("Search for comics", text: $searchText)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                        .padding(.horizontal, 30)
+                }
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(12)
+                .padding(.horizontal, 30)
+            }
+            .background(Color.black)
+            .onAppear {
+                rotate = true
+            }
+    
             
-            Text("O")
-                .foregroundStyle(.pink)
-                .fontWeight(.bold)
-            Text(".")
         }
     }
 }
 
 #Preview {
     Homepage()
+        .environment(DataManager())
 }
